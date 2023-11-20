efa2c61c06dc4637ae4ca692276c9d212c0aa548
Олег Ким <fyrl29074@gmail.com>
2023-11-20 18:07:50 +0300

    Change target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.fyrl29074.book_room_in_hotel"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fyrl29074.book_room_in_hotel"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
